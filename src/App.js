43c4ed9de0557c9a94b06d6e2d4f965b934ecd41
Xige Michael Chen <mxchen2001@gmail.com>
2022-08-07 13:56:10 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import CodeBlock from './Components/CodeBlock'; 
+
+const JSXCODE = `import React from 'react';
+import './App.css';
+
+function App() {
+  const a = 0;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Code Block
+      </header>
+      <CodeBlock />
+      <CodeBlock showLineNumbers={true} />
+      <header className="App-header">
+        More Coming
+      </header>
+    </div>
+  );
+}
+
+export default App;`;
+
+
+const CCODE = `#include <stdio.h>;
+
+int main() {
+	int counter = 0;
+
+	// printf() displays the string inside quotation
+	printf("Hello World!");
+
+	return 0;
+}`;
+
+const HTMLCODE = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="My Custom React Components"
+    />
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    <!--
+      manifest.json provides metadata used when your web app is installed on a
+      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+    -->
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    <!--
+      Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the \`public\` folder during the build.
+      Only files inside the \`public\` folder can be referenced from the HTML.
+
+      Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
+      work correctly both with client-side routing and a non-root public URL.
+      Learn how to configure a non-root public URL by running \`npm run build\`.
+    -->
+    <title>Flatter</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    <!--
+      This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the <body> tag.
+
+      To begin the development, run \`npm start\` or \`yarn start\`.
+      To create a production bundle, use \`npm run build\` or \`yarn build\`.
+    -->
+  </body>
+</html>
+`;
+
+const PYCODE = `def lev(s1, s2):
+  m = len(s1)
+  n = len(s2)
+
+  opt = [[-1 for _ in range(m + 1)] for _ in range(n + 1)]
+
+  #initialize base cases
+  for i in range(m + 1):
+      opt[0][i] = i
+  for j in range(n + 1):
+      opt[j][0] = j
+      
+  # fill in table with recursive case
+  for i in range(1,n+1):
+      for j in range(1,m+1):
+          # offset the index for 1-indexness of the table iteration
+          if s1[j-1] == s2[i-1]:
+              opt[i][j] = opt[i-1][j-1]
+          else:
+              minimum = min(opt[i-1][j], opt[i][j-1], opt[i-1][j-1])
+              opt[i][j] = minimum + 1
+          
+  return opt[n][m]`;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Code Block
+      </header>
+      <CodeBlock content={CCODE} language="c" title="HelloWorld.c"/>
+      <CodeBlock content={CCODE} language="c" showLineNumbers={true} title="With Line Numbers"/>
+      <CodeBlock content={JSXCODE} language="jsx" title="App.jsx"/>
+      <CodeBlock content={HTMLCODE} language="html" title="index.html"/>
+      <CodeBlock content={PYCODE} language="python" title="Levenshtein.py"/>
+      <header className="App-header">
+        More Coming
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CodeBlock.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import Prism from "prismjs";
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-jsx';
+
+
+function getTokens({ code, language }) {
+	const syntaxTree = Prism.tokenize(code + '\n\n', Prism.languages[language]);
+
+	// flatten the syntax tree into a single array of lines
+	const tokens = [];
+	let currentLine = [];
+
+	const recurse = (array, parentClass) => {
+		for (let i = 0; i < array.length; i++) {
+			const token = array[i];
+			if (Array.isArray(token.content)) {
+				const currentClass = parentClass.concat(token.type);
+				recurse(token.content, currentClass);
+			} else if (token.constructor.name === 'Token') {
+				token.type = parentClass.concat(token.type).join(' ')
+				currentLine.push(token);
+			} else {
+				// let parsedWhiteSpace = token.replace(/\t/g, '\u00A0\u00A0\u00A0\u00A0');
+				// parsedWhiteSpace = parsedWhiteSpace.replace(/ /g, "\u00A0");
+				let parsedWhiteSpace = token;
+
+				console.log(parsedWhiteSpace.split("\n"));
+				
+				const type = parentClass.length > 0 ? parentClass.join(' ') : '';
+				if (!parsedWhiteSpace.includes("\n")) {
+					
+					currentLine.push(new Prism.Token(type, parsedWhiteSpace, undefined));
+				} else {
+					parsedWhiteSpace.split(/\r?\n/).forEach((el, index) => {
+						// newline
+						currentLine.push(new Prism.Token(type, el, undefined));
+
+						// last split element is not a newline
+						if (index < parsedWhiteSpace.split(/\r?\n/).length - 1) {
+							tokens.push(currentLine);
+							currentLine = [];
+						}
+					});
+
+				}
+			}
+		}
+	}
+
+	recurse(syntaxTree, []);
+	console.log(tokens);
+
+	return tokens;
+}
+
+function Code({ tokens }) {
+	const renderSyntaxTreeEl = (el) => {
+		// if (el.content === '') {
+		// 	return <span>{"\u00A0"}</span>;
+		// }
+		return (
+			<span className={el.type}>
+				{el.content}
+			</span>
+		)
+	}
+
+	return (
+		<>
+			{
+				tokens.map((line, i) => {
+					return (
+						<pre className="code-line" role="presentation">
+							{
+							line.length == 1 && line[0].content === '' ? 
+								(<span>{"\n"}</span>) :
+								line.map((el, index) => renderSyntaxTreeEl(el))
+							}
+						</pre>
+					);
+				})
+			}
+		</>
+	);
+}
+
+function CodeBlock(props) {
+	const showLineNumbers = props.showLineNumbers || false;
+	const content = props.content || '';
+	const language = props.language || 'c';
+	const title = props.title || '';
+
+	console.log(content);
+	console.log(language);
+
+	const tokens = getTokens({ code: content, language: language });
+
+	return (
+		<div className="code-block-root">
+			<div className="code-block-wrapper">
+				<div className="code-block-top-bar">
+					<div className="circle-container">
+						<circle className="red" />
+						<circle className="yellow" />
+						<circle className="green" />
+					</div>
+					<title>{title}</title>
+				</div>
+				<div className="code-block-body-container">
+					<code className="line-numbers">
+						{showLineNumbers &&
+							<>
+								{tokens.map((line, i) =>	 (<span>{i+1}</span>))}
+							</>
+						}
+					</code>
+					<code className="code-block-body">
+						<Code tokens={tokens} />
+					</code>
+					<code className="right-border" />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CodeBlock;
